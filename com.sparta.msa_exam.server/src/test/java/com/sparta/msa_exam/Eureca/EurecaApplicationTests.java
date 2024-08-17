837f3fdfca24db643e7898c4d906f1c6ed268d9b
노민경 <142300528+minjjings@users.noreply.github.com>
2024-08-17 18:25:18 +0900

    server commit

---
+++ com.sparta.msa_exam.server/src/test/java/com/sparta/msa_exam/Eureca/EurecaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sparta.msa_exam.Eureca;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EurecaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
